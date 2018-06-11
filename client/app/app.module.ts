40055ad7bd986d0ffc69caf77eef8267cde58de7
WinfredMa <851936509@qq.com>
2018-06-11 21:19:03 +0800

    1. Update.

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -23,7 +23,7 @@ import { AccountComponent } from './account/account.component';
 import { AdminComponent } from './admin/admin.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NguiDatetimePickerModule } from '@ngui/datetime-picker';
-import {ChartModule} from "angular2-highcharts";
+import { ChartModule } from "angular2-highcharts";
 
 export function tokenGetter() {
   return localStorage.getItem('token');
